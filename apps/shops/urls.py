e9a42599a461a52382172a9912508ea8e78cb00b
hamedkhomjani <eng.h.khomjani@gmail.com>
2026-03-30 11:15:09 +0200

    feat: add under construction template and redirect dashboard view to it

--- apps/shops/urls.py
+++ apps/shops/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 app_name = 'shops'
 
 urlpatterns = [
-    # Dashboard section
-    path('dashboard/', views.dashboard, name='dashboard'),
+    # Dashboard section (Temporarily Under Construction)
+    # path('dashboard/', views.dashboard, name='dashboard'),
+    path('dashboard/', TemplateView.as_view(template_name='pages/under_construction.html'), name='dashboard'),
+    
     path('dashboard/register/', views.register_shop, name='register'),
     path('dashboard/add-price/', views.add_price, name='add_price'),
     path('dashboard/delete-price/<int:pk>/', views.delete_price, name='delete_price'),
